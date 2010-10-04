fd30d3b792146c5c32cbe97dd124b5f454695698
JensPenny <el.tutter@gmail.com>
2010-10-04 22:09:33 +0200

    The basic JSON network handler. Should be able to box blocks to JSON and unbox JSON to blocks. Needs a link to a network (socket) layer and a link to the repomanager.

---
+++ Master_proto1/src/penny/master/networking/JSONObjectManager.java
@@ -0,0 +1,82 @@
+package penny.master.networking;
+
+import java.lang.reflect.Constructor;
+
+import com.google.gson.Gson;
+
+import android.util.Log;
+
+import penny.master.blockbase.BaseBlock;
+
+public class JSONObjectManager {
+	
+    /**
+    *
+    * @param A baseblock
+    * @return An JSON - encoded string, ready for transmission
+    */
+   public static String getJsonObject(BaseBlock block){
+       //TODO: testen of blok een regelblok is: moeten anders geparset
+       boolean regelBlok = false;
+       String json = null;
+
+       if (regelBlok){
+
+       }else{
+           //TODO: Serieus hoort hier niet, maar anders nullpointerexept...
+           block.setStatusKlasseNaam(block.getStatus().getClass().getCanonicalName());
+           json = new Gson().toJson(block);
+       }
+       return json;
+   }
+
+   /**
+    *
+    * @param a JSON encoded string
+    * @return an object with the requested baseblock - inherited class
+    */
+   @SuppressWarnings("unchecked")
+public static Object decodeJSONObject(String input){
+       BaseBlock block = new Gson().fromJson(input, BaseBlock.class);
+       String klasse = block.getKlasse();
+       String statusKlasse = block.getStatusKlasseNaam();
+       Object result = null;
+
+        //TODO: locatie
+       try{
+           Class tocast = Class.forName(klasse);
+           Class statuscast = Class.forName(statusKlasse);
+           Class parameterType[] = new Class[2];
+           parameterType[0] = String.class; //2e paramtype in switch
+           Object argList[] = new Object[2];
+           argList[0] = block.getName(); //2e param in switch
+           parameterType[1] = statuscast;
+           argList[1] = Enum.valueOf(statuscast, block.getStatus().toString());
+//           if (tocast.equals(IMOutputBlock.class) || tocast.equals(PersoonSensorBlock.class)) //Beiden gebruiken imnotif --> is hier geen betere manier voor :/
+//           {
+//               parameterType[1] = IMSTATUS.class;
+//               argList[1] = IMSTATUS.valueOf(block.getStatus().toString());
+//           }
+           Constructor ct = tocast.getConstructor(parameterType);
+           result = ct.newInstance(argList);
+       }catch (ClassNotFoundException ex)
+       {
+           Log.e("Json Manager", "Could not find class with name " + ex.getMessage());
+           ex.printStackTrace();
+       }catch (ClassCastException ex){
+           Log.e("Json Manager","Could not cast the class");
+           ex.printStackTrace();
+       }catch (NoSuchMethodException ex){
+    	   Log.e("Json Manager","Could not find associated methods");
+           ex.printStackTrace();
+       }catch (InstantiationException ex){
+    	   Log.e("Json Manager","Could not instantiate new object");
+           ex.printStackTrace();
+       }catch (Throwable ex){
+    	   Log.e("Json Manager","An other error has occured");
+           ex.printStackTrace();
+       }
+       return result;
+   }
+
+}
